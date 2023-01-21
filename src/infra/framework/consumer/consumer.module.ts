b8fcfea299b108e7a9cd9a7442ff835c3775ce41
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-21 20:37:34 -0300

    feat: create ConsumerModule && setup its providers properly

---
+++ src/infra/framework/consumer/consumer.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+
+import { ConsumerController } from './consumer.controller';
+
+import { ConsumerRepository } from '@/app/repositories/consumer.repository';
+import { CreateConsumerUseCase } from '@/app/useCases/consumers/create-consumer.use-case';
+
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaConsumerRepository } from '@/infra/database/prisma/repositories/consumers.repository';
+
+@Module({
+  controllers: [ConsumerController],
+  providers: [
+    PrismaService,
+    {
+      provide: ConsumerRepository,
+      useFactory: (prismaService: PrismaService): PrismaConsumerRepository => new PrismaConsumerRepository(prismaService),
+      inject: [PrismaService]
+    },
+    {
+      provide: CreateConsumerUseCase,
+      useFactory: (repository: ConsumerRepository): CreateConsumerUseCase => new CreateConsumerUseCase(repository),
+      inject: [ConsumerRepository]
+    }
+  ]
+})
+export class ConsumerModule {}
