105ca7aba1d341e883bf45c3abddccfc8c863232
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-21 20:37:08 -0300

    feat: setup ConsumerController && create first method

---
+++ src/infra/framework/consumer/consumer.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { Consumer } from '@prisma/client';
+
+import { CreateConsumerUseCase } from '@/app/useCases/consumers/create-consumer.use-case';
+import { CreateConsumerDto } from '@/domain/dtos/consumer/create-consumer.dto';
+
+@Controller('consumers')
+export class ConsumerController {
+  constructor(private readonly createConsumerUseCase: CreateConsumerUseCase) {}
+
+  @Post()
+  public async create(@Body() createConsumerDto: CreateConsumerDto): Promise<Consumer> {
+    return this.createConsumerUseCase.execute(createConsumerDto);
+  }
+}
