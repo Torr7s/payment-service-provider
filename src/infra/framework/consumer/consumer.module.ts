708ecd52b7d3dcf5bc4280f5adf79b605e389ad8
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-21 20:43:54 -0300

    fix: import path

--- src/infra/framework/consumer/consumer.module.ts
+++ src/infra/framework/consumer/consumer.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConsumerController } from './consumer.controller';
 
 import { ConsumerRepository } from '@/app/repositories/consumer.repository';
-import { CreateConsumerUseCase } from '@/app/useCases/consumers/create-consumer.use-case';
+import { CreateConsumerUseCase } from '@/app/use-cases/consumers/create-consumer.use-case';
 
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { PrismaConsumerRepository } from '@/infra/database/prisma/repositories/consumers.repository';
