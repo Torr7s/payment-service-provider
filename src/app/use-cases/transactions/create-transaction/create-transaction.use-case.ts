5a06e9eeaa20d03817aed56fbad50a53fb85fe1a
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-23 19:02:37 -0300

    refactor: check if given payment method is valid

--- src/app/use-cases/transactions/create-transaction/create-transaction.use-case.ts
+++ src/app/use-cases/transactions/create-transaction/create-transaction.use-case.ts
@@ -1,3 +1,5 @@
+import { BadRequestException } from '@nestjs/common';
+
 import { TransactionRepository } from '@/app/abstracts/repositories/transaction.repository';
 
 import { ICreateTransactionUseCase, NSCreateTransactionUseCase } from '@/domain/use-cases/transactions';
@@ -6,6 +8,14 @@ export class CreateTransactionUseCase implements ICreateTransactionUseCase {
   constructor(private readonly repository: TransactionRepository) {}
 
   public async execute(params: NSCreateTransactionUseCase.Input): Promise<NSCreateTransactionUseCase.Output> {
+    const validPaymentMethods: string[] = ['CREDIT_CARD', 'DEBIT_CARD'];
+
+    if (!validPaymentMethods.includes(params.paymentMethod)) {
+      throw new BadRequestException('Invalid transaction', {
+        description: `A valid payment method wasnt given, acceptable methods: ${validPaymentMethods.join(', ')}`
+      });
+    }
+
     return this.repository.create({
       ...params,
       cardNumber: params.cardNumber.slice(-4)
