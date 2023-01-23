5321bdde902c23772ceb704976d58826859c4158
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-23 10:36:55 -0300

    feat: decorate CreateTransactionDto props

--- src/domain/dtos/transaction/create-transaction.dto.ts
+++ src/domain/dtos/transaction/create-transaction.dto.ts
@@ -1,12 +1,50 @@
-import { PaymentMethod } from '@prisma/client';
-import { Decimal } from '@prisma/client/runtime';
+import { Prisma, PaymentMethod } from '@prisma/client';
+import { 
+  IsDateString, 
+  IsDecimal, 
+  IsNotEmpty, 
+  IsString, 
+  IsUUID, 
+  MaxLength,
+  MinLength 
+} from 'class-validator';
+
+import { CheckPaymentMethod } from '@/core/@decorators';
 
 export class CreateTransactionDto {
-  value: string | Decimal;
+  @IsDecimal()
+  @IsString()
+  @IsNotEmpty()
+  value: string | Prisma.Decimal;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+  
+  @CheckPaymentMethod(PaymentMethod, { each: true })
+  @IsNotEmpty()
   paymentMethod: PaymentMethod;
+  
+  @IsString()
+  @MinLength(16)
+  @MaxLength(16)
+  @IsNotEmpty()
   cardNumber: string;
+  
+  @IsString()
+  @IsNotEmpty()
   cardOwnerName: string;
+  
+  @IsDateString()
+  @IsNotEmpty()
   cardExpirationDate: string | Date;
+  
+  @IsString()
+  @MinLength(3)
+  @MaxLength(3)
+  @IsNotEmpty()
   cardCVV: string;
+
+  @IsUUID()
+  consumerId: string;
 }
