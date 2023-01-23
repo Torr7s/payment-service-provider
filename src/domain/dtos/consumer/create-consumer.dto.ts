8c70bf8469055a6d3ea1f37cdae6483d5506909a
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-23 17:37:00 -0300

    feat: add password prop && decorate accordingly

--- src/domain/dtos/consumer/create-consumer.dto.ts
+++ src/domain/dtos/consumer/create-consumer.dto.ts
@@ -1,4 +1,15 @@
-export type CreateConsumerDto = {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateConsumerDto {
+  @IsString()
+  @IsNotEmpty()
   fullName: string;
+  
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 }
