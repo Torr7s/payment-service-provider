d3ca6c553896227601ca604d0687410231632ffc
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-24 15:40:23 -0300

    feat: setup ConfigModule

--- src/infra/framework/nest/app.module.ts
+++ src/infra/framework/nest/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config';
 
 import { ConsumerModule } from './modules/consumer/consumer.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true
+    }),
     ConsumerModule,
     TransactionModule
   ]
