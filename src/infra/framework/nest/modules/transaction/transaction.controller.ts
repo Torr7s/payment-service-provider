87894ba928439942d5c1c8a9c32d151ed15160f9
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-24 10:26:45 -0300

    feat: add route for listing consumer transactions

--- src/infra/framework/nest/modules/transaction/transaction.controller.ts
+++ src/infra/framework/nest/modules/transaction/transaction.controller.ts
@@ -2,19 +2,30 @@ import { Transaction } from '@prisma/client';
 import { 
   Body, 
   Controller, 
+  Get, 
+  Param, 
+  ParseUUIDPipe, 
   Post 
 } from '@nestjs/common';
 
-import { CreateTransactionUseCase } from '@/app/use-cases/transactions';
+import { CreateTransactionUseCase, ListTransactionsUseCase } from '@/app/use-cases/transactions';
 
 import { CreateTransactionDto } from '@/domain/dtos/transaction/create-transaction.dto';
 
 @Controller('transactions')
 export class TransactionController {
-  constructor(private readonly createTransactionUseCase: CreateTransactionUseCase) {}
+  constructor(
+    private readonly createTransactionUseCase: CreateTransactionUseCase,
+    private readonly listTransactionsUseCase: ListTransactionsUseCase
+  ) {}
 
   @Post()
   public async create(@Body() createTransactionDto: CreateTransactionDto): Promise<Transaction> {
     return this.createTransactionUseCase.execute(createTransactionDto);
+  }
+
+  @Get(':consumerId')
+  public async list(@Param('consumerId', new ParseUUIDPipe()) consumerId: string): Promise<Transaction[]> {
+    return this.listTransactionsUseCase.execute({ consumerId });
   }
 }
