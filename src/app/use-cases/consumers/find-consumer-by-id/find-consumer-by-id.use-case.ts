815f7ced66ec1032e14098c6e3698e6c67fe5cdc
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-25 20:31:27 -0300

    fix: import paths && methods' param

--- src/app/use-cases/consumers/find-consumer-by-id/find-consumer-by-id.use-case.ts
+++ src/app/use-cases/consumers/find-consumer-by-id/find-consumer-by-id.use-case.ts
@@ -1,15 +1,15 @@
 import { Consumer } from '@prisma/client';
 import { BadRequestException } from '@nestjs/common';
 
-import { IFindConsumerByIdUseCase, NSFindConsumerByIdUseCase } from '@/domain/use-cases/consumers';
+import { IFindConsumerByIdUseCase } from '@/domain/use-cases/consumers';
 
-import { ConsumerRepository } from '@/app/abstracts/repositories';
+import { ConsumerRepository } from '@/app/abstracts/repositories/consumer.repository';
 
 export class FindConsumerByIdUseCase implements IFindConsumerByIdUseCase {
   constructor(private readonly repository: ConsumerRepository) {}
 
-  public async execute({ id }: NSFindConsumerByIdUseCase.Input): Promise<NSFindConsumerByIdUseCase.Output> {
-    const consumer: Consumer = await this.repository.findById({ id });
+  public async exec(id: string): Promise<Consumer> {
+    const consumer: Consumer = await this.repository.findById(id);
 
     if (!consumer) {
       throw new BadRequestException('Invalid consumer', {
