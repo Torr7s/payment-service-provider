feba8e943f94e9aa2d7c63930ab2fff759953c49
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-26 00:09:45 -0300

    refactor: import module providers

--- src/infra/framework/nest/modules/transaction/transaction.module.ts
+++ src/infra/framework/nest/modules/transaction/transaction.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { TransactionController } from './transaction.controller';
 
-import { TransactionRepository } from '@/app/abstracts/repositories/transaction.repository';
-
-import { CreateTransactionUseCase } from '@/app/use-cases/transactions/create-transaction';
-import { ListTransactionsUseCase } from '@/app/use-cases/transactions/list-transactions';
-
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
-import { PrismaTransactionRepository } from '@/infra/database/prisma/repositories/transaction.repository';
+import { TransactionModuleProviders } from '../../ioc/providers/transaction.providers';
 
 @Module({
   controllers: [TransactionController],
-  providers: [
-
-  ]
+  providers: TransactionModuleProviders
 })
 export class TransactionModule {}
