afc58f825bad83ba5c42ac889a53d63129553a78
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-25 21:25:23 -0300

    fix: import paths

--- src/infra/framework/nest/modules/transaction/transaction.module.ts
+++ src/infra/framework/nest/modules/transaction/transaction.module.ts
@@ -3,7 +3,9 @@ import { Module } from '@nestjs/common';
 import { TransactionController } from './transaction.controller';
 
 import { TransactionRepository } from '@/app/abstracts/repositories/transaction.repository';
-import { CreateTransactionUseCase, ListTransactionsUseCase } from '@/app/use-cases/transactions';
+
+import { CreateTransactionUseCase } from '@/app/use-cases/transactions/create-transaction';
+import { ListTransactionsUseCase } from '@/app/use-cases/transactions/list-transactions';
 
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { PrismaTransactionRepository } from '@/infra/database/prisma/repositories/transaction.repository';
