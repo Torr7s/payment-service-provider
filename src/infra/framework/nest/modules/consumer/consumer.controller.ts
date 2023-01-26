032ad56bccc568bd964da7ce9734ae8c56bcd658
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-25 21:26:39 -0300

    feat: add route to search consumer by id

--- src/infra/framework/nest/modules/consumer/consumer.controller.ts
+++ src/infra/framework/nest/modules/consumer/consumer.controller.ts
@@ -1,4 +1,19 @@
-import { Controller} from '@nestjs/common';
+import { Consumer } from '@prisma/client';
+import { 
+  Controller, 
+  Get, 
+  Param, 
+  ParseUUIDPipe
+} from '@nestjs/common';
+
+import { FindConsumerByIdUseCase } from '@/app/use-cases/consumers/find-consumer-by-id';
 
 @Controller('consumers')
-export class ConsumerController {}
+export class ConsumerController {
+  constructor(private readonly findConsumerByIdUseCase: FindConsumerByIdUseCase) {}
+
+  @Get(':consumerId')
+  public async findById(@Param('consumerId', new ParseUUIDPipe()) consumerId: string): Promise<Consumer> {
+    return this.findConsumerByIdUseCase.exec(consumerId);
+  }
+}
