2d01d84bbc4427de8790f62aec38d6b2bb7058a0
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-25 21:27:52 -0300

    feat: add FindConsumerByIdUseCase as a provider

--- src/infra/framework/nest/modules/consumer/consumer.module.ts
+++ src/infra/framework/nest/modules/consumer/consumer.module.ts
@@ -4,6 +4,8 @@ import { ConsumerController } from './consumer.controller';
 
 import { ConsumerRepository } from '@/app/abstracts/repositories/consumer.repository';
 
+import { FindConsumerByIdUseCase } from '@/app/use-cases/consumers/find-consumer-by-id';
+
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { PrismaConsumerRepository } from '@/infra/database/prisma/repositories/consumer.repository';
 
@@ -15,6 +17,11 @@ import { PrismaConsumerRepository } from '@/infra/database/prisma/repositories/c
       provide: ConsumerRepository,
       useFactory: (prismaService: PrismaService): PrismaConsumerRepository => new PrismaConsumerRepository(prismaService),
       inject: [PrismaService]
+    },
+    {
+      provide: FindConsumerByIdUseCase,
+      useFactory: (consumerRepository: ConsumerRepository): FindConsumerByIdUseCase => new FindConsumerByIdUseCase(consumerRepository),
+      inject: [ConsumerRepository]
     }
   ]
 })
