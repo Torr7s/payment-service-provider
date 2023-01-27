a409f319b163c510c000165a53effb5c218edc2f
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-27 12:15:46 -0300

    feat: remove AppModuleProviders && import AuthModule

--- src/infra/framework/nest/app.module.ts
+++ src/infra/framework/nest/app.module.ts
@@ -1,23 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-
-import { AppModuleProviders } from './ioc/providers/app.providers';
-
+import { AuthModule } from './modules/auth/auth.module';
 import { ConsumerModule } from './modules/consumer/consumer.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true
     }),
+    AuthModule,
     ConsumerModule,
     TransactionModule
-  ],
-  providers: AppModuleProviders
+  ]
 })
 export class AppModule {}
