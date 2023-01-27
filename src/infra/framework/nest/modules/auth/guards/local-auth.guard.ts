c1a8b086ea4b440ab096f2d53685b16c07e33254
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-27 12:10:36 -0300

    feat: create & setup auth guards

---
+++ src/infra/framework/nest/modules/auth/guards/local-auth.guard.ts
@@ -0,0 +1,18 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const result = (await super.canActivate(context)) as boolean;
+
+    if (context.getType() === 'http') {
+      const request = context.switchToHttp().getRequest();
+
+      await super.logIn(request);
+    }
+
+    return result;
+  }
+}
