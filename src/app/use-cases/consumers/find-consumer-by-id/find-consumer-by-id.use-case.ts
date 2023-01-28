37245c303656f05b947230a1229203185a721082
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-28 20:40:44 -0300

    style: move import up

--- src/app/use-cases/consumers/find-consumer-by-id/find-consumer-by-id.use-case.ts
+++ src/app/use-cases/consumers/find-consumer-by-id/find-consumer-by-id.use-case.ts
@@ -1,9 +1,9 @@
 import { Consumer } from '@prisma/client';
 import { BadRequestException } from '@nestjs/common';
 
-import { IFindConsumerByIdUseCase } from '@/domain/use-cases/consumers';
-
 import { ConsumerRepository } from '@/app/abstracts/repositories/consumer.repository';
+
+import { IFindConsumerByIdUseCase } from '@/domain/use-cases/consumers';
 
 export class FindConsumerByIdUseCase implements IFindConsumerByIdUseCase {
   constructor(private readonly repository: ConsumerRepository) {}
