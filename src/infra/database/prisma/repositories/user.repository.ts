1a617c75254d7dfcf370b4d1ba10ee77a2887320
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-28 16:00:23 -0300

    feat: create private _include for find methods to include

--- src/infra/database/prisma/repositories/user.repository.ts
+++ src/infra/database/prisma/repositories/user.repository.ts
@@ -5,13 +5,22 @@ import { PrismaService } from '../prisma.service';
 import { UserRepository } from '@/app/abstracts/repositories/user.repository';
 
 export class PrismaUserRepository implements UserRepository {
+  private readonly _include = {
+    consumerProfile: {
+      select: {
+        id: true
+      }
+    }
+  }
+  
   constructor(private prismaService: PrismaService) {}
 
   public async findByEmail(email: string): Promise<User> {
     return this.prismaService.user.findUnique({
       where: {
         email
-      }
+      },
+      include: this._include
     });
   }
 
@@ -19,7 +28,8 @@ export class PrismaUserRepository implements UserRepository {
     return this.prismaService.user.findUnique({
       where: {
         id
-      }
+      },
+      include: this._include
     });
   }
 }
