34064751df768e7f90ed5732b077e88ca50a6652
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-28 20:35:37 -0300

    feat: apply guards on route && make use of AuthUser to retrieve user

--- src/infra/framework/nest/modules/transaction/transaction.controller.ts
+++ src/infra/framework/nest/modules/transaction/transaction.controller.ts
@@ -1,19 +1,28 @@
 import { Transaction } from '@prisma/client';
-import { 
-  Body, 
-  Controller, 
-  Get, 
-  Param, 
-  ParseUUIDPipe, 
-  Post 
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
+
+import { AuthUser } from '../auth/decorators/auth-user.decorator';
+
+import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 import { CreateTransactionUseCase } from '@/app/use-cases/transactions/create-transaction';
 import { ListTransactionsUseCase } from '@/app/use-cases/transactions/list-transactions';
 
 import { CreateTransactionDto } from '@/domain/dtos/transaction/create-transaction.dto';
 
+import { UserEntity } from '@/domain/entities/user.entity';
+
 @Controller('transactions')
+@UseGuards(SessionAuthGuard, JwtAuthGuard)
 export class TransactionController {
   constructor(
     private readonly createTransactionUseCase: CreateTransactionUseCase,
@@ -21,12 +30,20 @@ export class TransactionController {
   ) {}
 
   @Post()
-  public async create(@Body() createTransactionDto: CreateTransactionDto): Promise<Transaction> {
-    return this.createTransactionUseCase.exec(createTransactionDto);
+  @HttpCode(HttpStatus.CREATED)
+  public async create(
+    @AuthUser() user: UserEntity,
+    @Body() createTransactionDto: CreateTransactionDto
+  ): Promise<Transaction> {
+    return this.createTransactionUseCase.exec({
+      ...createTransactionDto,
+      consumerId: user.consumerProfile.id
+    });
   }
 
-  @Get(':consumerId')
-  public async list(@Param('consumerId', new ParseUUIDPipe()) consumerId: string): Promise<Transaction[]> {
-    return this.listTransactionsUseCase.exec(consumerId);
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  public async list(@AuthUser() user: UserEntity): Promise<Transaction[]> {
+    return this.listTransactionsUseCase.exec(user.consumerProfile.id);
   }
 }
