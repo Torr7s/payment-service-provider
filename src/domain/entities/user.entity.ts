34ef0ce4c3e931573d971c9fa60b53b159189d38
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-28 15:46:40 -0300

    feat: type consumerProfile as ConsumerEntity properly

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,9 +1,11 @@
+import { ConsumerEntity } from './consumer.entity';
+
 export class UserEntity {
   id?: string;
   fullName: string;
   email: string;
   password: string;
-  consumerProfile?: object;
+  consumerProfile?: ConsumerEntity;
   createdAt?: string | Date;
   updatedAt?: string | Date;
 }
