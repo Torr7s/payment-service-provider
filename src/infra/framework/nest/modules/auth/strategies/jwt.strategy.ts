a1b4f0191da7015e0c67b3370ca5a62061af0ea1
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-29 11:14:32 -0300

    feat: set ignoreExpiration as true

--- src/infra/framework/nest/modules/auth/strategies/jwt.strategy.ts
+++ src/infra/framework/nest/modules/auth/strategies/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET_KEY,
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       passReqToCallback: false
     });
   }
