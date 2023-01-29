7896234afd6dc2c306f38cc72e9282e7b37d54a3
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-29 15:35:02 -0300

    refactor: add consumerId & transactionId props

--- src/domain/entities/payable.entity.ts
+++ src/domain/entities/payable.entity.ts
@@ -6,11 +6,13 @@ import { TransactionEntity } from './transaction.entity';
 
 export class PayableEntity {
   id?: string;
-  status?: PayableStatus;
-  paymentDate?: string | Date;
-  fee?: string | number | Prisma.Decimal | DecimalJsLike;
-  consumer: ConsumerEntity;
-  transaction: TransactionEntity;
+  status: PayableStatus;
+  paymentDate: string | Date;
+  fee: string | number | Prisma.Decimal | DecimalJsLike;
+  consumer?: ConsumerEntity;
+  consumerId: string;
+  transaction?: TransactionEntity;
+  transactionId: string;
   createdAt?: string | Date;
   updatedAt?: string | Date;
 }
