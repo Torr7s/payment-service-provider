627a0075b3f1085ef2a864bc9e4bb5f4b8370bbf
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-30 11:13:11 -0300

    feat: delete consumer module

--- src/infra/framework/modules/app.module.ts
+++ src/infra/framework/modules/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
-import { ConsumerModule } from './consumer/consumer.module';
 import { PayableModule } from './payable/payable.module';
 import { TransactionModule } from './transaction/transaction.module';
 
@@ -13,7 +12,6 @@ import { TransactionModule } from './transaction/transaction.module';
       isGlobal: true
     }),
     AuthModule,
-    ConsumerModule,
     TransactionModule,
     PayableModule
   ]
