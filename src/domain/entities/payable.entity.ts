dbeb845ced54506d34268970138672588aecf743
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-29 21:13:55 -0300

    refactor: remove consumer & transaction props

--- src/domain/entities/payable.entity.ts
+++ src/domain/entities/payable.entity.ts
@@ -2,15 +2,11 @@ import { PayableStatus, Prisma } from '@prisma/client';
 import { DecimalJsLike } from '@prisma/client/runtime';
 
 import { BaseEntity } from './base.entity';
-import { ConsumerEntity } from './consumer.entity';
-import { TransactionEntity } from './transaction.entity';
 
 export class PayableEntity extends BaseEntity {
   status: PayableStatus;
   paymentDate: string | Date;
   fee: string | number | Prisma.Decimal | DecimalJsLike;
-  consumer?: ConsumerEntity;
   consumerId: string;
-  transaction?: TransactionEntity;
   transactionId: string;
 }
