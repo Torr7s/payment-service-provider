5acaaf3879c16cfa4ca30c36ec9d0c74de93d375
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-29 21:25:24 -0300

    feat: add needed providers for creating payables

--- src/infra/framework/nest/modules/transaction/transaction.module.ts
+++ src/infra/framework/nest/modules/transaction/transaction.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { TransactionController } from './transaction.controller';
 
+import { PayableRepository } from '@/app/abstracts/repositories/payable.repository';
 import { TransactionRepository } from '@/app/abstracts/repositories/transaction.repository';
 import { UserRepository } from '@/app/abstracts/repositories/user.repository';
 
 import { AuthUseCase } from '@/app/use-cases/auth/auth';
+import { CreatePayableUseCase } from '@/app/use-cases/payables/create-payable';
 
 import { CreateTransactionUseCase } from '@/app/use-cases/transactions/create-transaction';
 import { ListTransactionsUseCase } from '@/app/use-cases/transactions/list-transactions';
 
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 
+import { PrismaPayableRepository } from '@/infra/database/prisma/repositories/payable.repository';
 import { PrismaTransactionRepository } from '@/infra/database/prisma/repositories/transaction.repository';
 import { PrismaUserRepository } from '@/infra/database/prisma/repositories/user.repository';
 
@@ -20,8 +23,14 @@ import { PrismaUserRepository } from '@/infra/database/prisma/repositories/user.
   providers: [
     PrismaService,
     {
+      provide: PayableRepository,
+      useFactory: (prismaService: PrismaService): PrismaPayableRepository =>
+        new PrismaPayableRepository(prismaService),
+      inject: [PrismaService]
+    },
+    {
       provide: TransactionRepository,
-      useFactory: (prismaService: PrismaService): PrismaTransactionRepository => 
+      useFactory: (prismaService: PrismaService): PrismaTransactionRepository =>
         new PrismaTransactionRepository(prismaService),
       inject: [PrismaService]
     },
@@ -33,19 +42,31 @@ import { PrismaUserRepository } from '@/infra/database/prisma/repositories/user.
     },
     {
       provide: AuthUseCase,
-      useFactory: (userRepository: UserRepository): AuthUseCase => 
+      useFactory: (userRepository: UserRepository): AuthUseCase =>
         new AuthUseCase(userRepository),
       inject: [UserRepository]
     },
     {
+      provide: CreatePayableUseCase,
+      useFactory: (payableRepository: PayableRepository, transactionRepository: TransactionRepository): CreatePayableUseCase =>
+        new CreatePayableUseCase(
+          payableRepository,
+          transactionRepository
+        ),
+      inject: [PayableRepository, TransactionRepository]
+    },
+    {
       provide: CreateTransactionUseCase,
-      useFactory: (transactionRepository: TransactionRepository): CreateTransactionUseCase => 
-        new CreateTransactionUseCase(transactionRepository),
-      inject: [TransactionRepository]
+      useFactory: (createPayableUseCase: CreatePayableUseCase, transactionRepository: TransactionRepository): CreateTransactionUseCase =>
+        new CreateTransactionUseCase(
+          createPayableUseCase,
+          transactionRepository
+        ),
+      inject: [CreatePayableUseCase, TransactionRepository]
     },
     {
       provide: ListTransactionsUseCase,
-      useFactory: (transactionRepository: TransactionRepository): ListTransactionsUseCase => 
+      useFactory: (transactionRepository: TransactionRepository): ListTransactionsUseCase =>
         new ListTransactionsUseCase(transactionRepository),
       inject: [TransactionRepository]
     }
