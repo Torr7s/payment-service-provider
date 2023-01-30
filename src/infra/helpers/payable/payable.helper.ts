e7d2dfc1b682ba5962e0a97e7985d60e418c6ecb
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-29 21:18:27 -0300

    feat: create calculateFee as a payable helper

---
+++ src/infra/helpers/payable/payable.helper.ts
@@ -0,0 +1,23 @@
+import { PaymentMethod, Prisma } from '@prisma/client';
+
+import { TransactionEntity } from '@/domain/entities/transaction.entity';
+
+enum PaymentMethodFee {
+  CREDIT_CARD = 0.05,
+  DEBIT_CARD = 0.03
+}
+
+export const calculateFee = (transaction: TransactionEntity): Prisma.Decimal => {
+  const paymentMethod: string = transaction.paymentMethod;
+  const value: number = Number(transaction.value);
+
+  if (paymentMethod === PaymentMethod.credit_card) {
+    const creditDiscount: number = PaymentMethodFee.CREDIT_CARD;
+
+    return new Prisma.Decimal(value - (value * creditDiscount));
+  } else if (paymentMethod === PaymentMethod.debit_card) {
+    const debitDiscount: number = PaymentMethodFee.DEBIT_CARD;
+
+    return new Prisma.Decimal(value - (value * debitDiscount));
+  }
+}
