798b76076f72cc557d21cf2015481e63ecba1700
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-31 12:20:05 -0300

    feat: use global filters && import sessionConfig

--- src/setup.ts
+++ src/setup.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-import session from 'express-session';
 import passport from 'passport';
-import connectPgSimple from 'connect-pg-simple';
 import cookieParser from 'cookie-parser';
+
+import sessionConfig from './config/session';
+
+import { HttpExceptionFilter } from './infra/framework/exceptions/exception.filter';
 
 export function setup(app: INestApplication): INestApplication {
   app.useGlobalPipes(
@@ -12,26 +14,13 @@ export function setup(app: INestApplication): INestApplication {
       forbidNonWhitelisted: true
     })
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(sessionConfig);
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.use(cookieParser(process.env.JWT_SECRET_KEY));
-
-  app.use(
-    session({
-      secret: process.env.JWT_SECRET_KEY as string,
-      resave: false,
-      saveUninitialized: false,
-      store: process.env.STAGE === 'production' ? new (connectPgSimple(session))() : new session.MemoryStore(),
-      cookie: {
-        httpOnly: true,
-        signed: true,
-        sameSite: 'strict',
-        secure: process.env.STAGE === 'production'
-      }
-    })
-  );
-
-  app.use(passport.initialize())
-  app.use(passport.session());
 
   return app;
 }
