a208acada1eb42607ecada09d96d060271b9482f
Matheus Torres <torresmiranda.rj@gmail.com>
2023-02-01 17:48:41 -0300

    chore: delete abstract repository

--- src/app/abstracts/repositories/auth.repository.ts
+++
@@ -1,7 +0,0 @@
-import { UserEntity } from '@/domain/entities/user.entity';
-
-import { IAuthenticationRepository } from '@/domain/repositories/authentication.repository';
-
-export abstract class AuthRepository implements IAuthenticationRepository {
-  public abstract signUp: (data: UserEntity) => Promise<UserEntity>;
-}
