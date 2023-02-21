0bfae885e328e861aa73aeae3e7af46c263af4d0
Matheus Torres <torresmiranda.rj@gmail.com>
2023-02-21 17:55:04 -0300

    feat: create DatabaseModule && setup module metadata

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaService } from './prisma/prisma.service';
+
+import { PayableRepository } from '@/app/abstracts/repositories/payable.repository';
+import { TransactionRepository } from '@/app/abstracts/repositories/transaction.repository';
+import { UserRepository } from '@/app/abstracts/repositories/user.repository';
+
+import { PrismaPayableRepository } from './prisma/repositories/payable.repository';
+import { PrismaTransactionRepository } from './prisma/repositories/transaction.repository';
+import { PrismaUserRepository } from './prisma/repositories/user.repository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: PayableRepository,
+      useClass: PrismaPayableRepository,
+    },
+    {
+      provide: TransactionRepository,
+      useClass: PrismaTransactionRepository,
+    },
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository
+    }
+  ],
+  exports: [
+    UserRepository
+  ]
+})
+export class DatabaseModule {}
