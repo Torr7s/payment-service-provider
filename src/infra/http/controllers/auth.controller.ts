c9176caceeb16de931a340af13dfcab12840dab5
Matheus Torres <torresmiranda.rj@gmail.com>
2023-03-11 23:55:39 -0300

    feat: map user to its dto properly

--- src/infra/http/controllers/auth.controller.ts
+++ src/infra/http/controllers/auth.controller.ts
@@ -17,9 +17,11 @@ import { JwtAuthGuard } from '../core/auth/guards/jwt-auth.guard';
 
 import { TokenInterceptor } from '../core/auth/interceptors/token.interceptor';
 
-import { SignUpDto } from '../dtos/authentication';
+import { SignUpDto, UserDto } from '../dtos/authentication';
+import { UserMapper } from '../mappers/user.mapper';
 
-import { UserEntity } from '@/src/app/entities/user.entity';
+
+import { User } from '@/src/app/entities/user';
 import { AuthSignUpUseCase } from '@/src/app/use-cases/auth/sign-up';
 
 @Controller('auth')
@@ -30,20 +32,28 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   @UseInterceptors(TokenInterceptor)
-  public async signIn(@AuthUser() user: UserEntity): Promise<UserEntity> {
-    return user;
+  public async signIn(@AuthUser() user: User): Promise<{ user: UserDto }> {
+    return {
+      user: UserMapper.toDto(user)
+    }
   }
 
   @Post('signup') 
   @HttpCode(HttpStatus.CREATED)
-  public async signUp(@Body() signUpDto: SignUpDto): Promise<{ user: UserEntity }> {
-    return this.authSignUpUseCase.exec(signUpDto);
+  public async signUp(@Body() signUpDto: SignUpDto): Promise<{ user: UserDto }> {
+    const { user } = await this.authSignUpUseCase.exec(signUpDto);
+
+    return {
+      user: UserMapper.toDto(user)
+    }
   }
 
   @Get('me')
   @HttpCode(HttpStatus.OK)
   @UseGuards(SessionAuthGuard, JwtAuthGuard)
-  public me(@AuthUser() user: UserEntity): UserEntity {
-    return user;
+  public me(@AuthUser() user: User): { user: UserDto } {
+    return {
+      user: UserMapper.toDto(user)
+    }
   }
 } 
